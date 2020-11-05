327c831fc342ecc9cc0c5b27955922407dbd29d1
Zhang269u <70938138+Zhang269u@users.noreply.github.com>
2020-11-05 10:53:21 +0100

    Correction créeRedacteur

--- CreeRedacteur.php
+++ CreeRedacteur.php
@@ -1,6 +1,69 @@
 <?php
 require_once ('Connexion.php');
 error_reporting(E_ALL ^E_NOTICE);
+$Terreur = [];
+if (isset($_POST['Valider'])) {
+    $Terreur['nom'] = 'A saisir';
+    $nom = '';
+    if (isset($_POST['nom'])) {
+        $nom = $_POST['nom'];
+        if (!empty(trim($nom))){
+            if (!preg_match("#^[A-z]+[\-']?[[a-z]+[\-']?]*[a-z]+$#",$_POST['nom']))
+                $Terreur['nom'] = 'nom non valide';
+            else
+                $Terreur['nom'] = '';
+        }
+    }
+
+    $Terreur['prenom'] = 'A saisir';
+    $prenom = '';
+    if (isset($_POST['prenom'])) {
+        $prenom = $_POST['prenom'];
+        if (!empty(trim($prenom)))
+            if (!preg_match("#^[A-z]+[\-']?[[a-z]+[\-']?]*[a-z]+$#",$_POST['prenom']))
+                $Terreur['prenom'] = 'prenom non valide';
+            else
+                $Terreur['prenom'] = '';
+    }
+
+    $Terreur['email'] = 'A saisir';
+    $email = '';
+    if (isset($_POST['email'])) {
+        $email = $_POST['email'];
+        if (!empty(trim($email)))
+            if (!preg_match("#^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$#",$_POST['email']))
+                $Terreur['email'] = 'email non valide';
+            else
+                $Terreur['email'] = '';
+    }
+
+    $Terreur['password'] = 'A saisir';
+    $password = '';
+    if (isset($_POST['password'])) {
+        $password = $_POST['password'];
+        if (!empty(trim($password))) {
+            $confirmpassword = '';
+            if (isset($_POST['confirmpassword'])) {
+                $confirmpassword = $_POST['confirmpassword'];
+                if ($password == $confirmpassword)
+                    $Terreur['password'] = '';
+                else
+                    $Terreur['password'] = 'Les mots de passe sont différent';
+            }
+        }
+    }
+
+    if ($Terreur['nom'] == '' && $Terreur['prenom'] == '' && $Terreur['email'] == '' && $Terreur['password'] == '') {
+        $insert_stmt = $objPdo->prepare('INSERT INTO redacteur (nom, prenom, adressemail, motdepasse) VALUES (?,?,?,?)');
+        $insert_stmt->bindValue(1, $nom, PDO::PARAM_STR);
+        $insert_stmt->bindValue(2, $prenom, PDO::PARAM_STR);
+        $insert_stmt->bindValue(3, $email, PDO::PARAM_STR);
+        $insert_stmt->bindValue(4, $password, PDO::PARAM_STR);
+        $insert_stmt->execute();
+        header("Location:News.php");
+    }
+
+}
 
 ?>
 <html>
@@ -28,6 +91,8 @@ error_reporting(E_ALL ^E_NOTICE);
     }
     function checkPassword(password,password2)
     {
+        if(password=="")
+            return false;
         return password==password2;
     }
     function validate() {
@@ -36,85 +101,22 @@ error_reporting(E_ALL ^E_NOTICE);
         var prenom = document.getElementById("prenom").value;
         var password = document.getElementById("password").value;
         var confirmpassword = document.getElementById("confirmpassword").value;
-        var valid=true;
         if (!checkNom(nom)) {
             alert('nom non valide');
-            valid=false;
         }
         if (!checkPrenom(prenom)) {
             alert('prenom non valide');
-            valid=false;
         }
         if (!checkEmail(email)) {
             alert('Adresse e-mail non valide');
-            valid=false;
         }
         if(!checkPassword(password,confirmpassword)){
             alert('Mot de passe different ou non rempli');
-            valid=false;
         }
-        return valid;
+        return false;
     }
 </script>
     <body>
-    <?php
-    $Terreur=[];
-
-    if(isset($_POST['Valider'])){
-        $valid = '<script type="text/javascript">document.write(validate());</script>';
-        $Terreur['nom']='A saisir';
-        $nom='';
-        if(isset($_POST['nom'])){
-            $nom=$_POST['nom'];
-            if(!empty(trim($nom)))
-                $Terreur['nom']='';
-        }
-
-        $Terreur['prenom']='A saisir';
-        $prenom='';
-        if(isset($_POST['prenom'])){
-            $prenom=$_POST['prenom'];
-            if(!empty(trim($prenom)))
-                $Terreur['prenom']='';
-        }
-
-        $Terreur['email']='A saisir';
-        $email='';
-        if(isset($_POST['email'])){
-            $email=$_POST['email'];
-            if(!empty(trim($email)))
-                $Terreur['email']='';
-        }
-
-        $Terreur['password']='A saisir';
-        $password='';
-        if(isset($_POST['password'])){
-            $password=$_POST['password'];
-            if(!empty(trim($password))){
-                $confirmpassword='';
-                if(isset($_POST['confirmpassword'])) {
-                    $confirmpassword = $_POST['confirmpassword'];
-                    if ($password == $confirmpassword)
-                        $Terreur['password'] = '';
-                    else
-                        $Terreur['password'] = 'Les mots de passe sont différent';
-                }
-            }
-        }
-
-        if($Terreur['nom']=='' && $Terreur['prenom']=='' && $Terreur['email']=='' && $Terreur['password']==''){
-            echo 'haha';
-            $insert_stmt = $objPdo->prepare('INSERT INTO redacteur (nom, prenom, adressemail, motdepasse) VALUES (?,?,?,?)');
-            $insert_stmt->bindValue(1, $nom, PDO::PARAM_STR);
-            $insert_stmt->bindValue(2, $prenom, PDO::PARAM_STR);
-            $insert_stmt->bindValue(3, $email, PDO::PARAM_STR);
-            $insert_stmt->bindValue(4, $password, PDO::PARAM_STR);
-            $insert_stmt->execute();
-            header("Location:News.php");
-        }
-
-    }
-    ?>
         <h1>Saisie des Informations du Redacteur</h1>
         <form method='post' action=''>
             <label for='nom'>Nom :</label>
